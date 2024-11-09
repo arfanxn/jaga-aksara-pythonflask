690cbf82e5959a7357b438dcc81df2f4f1187d1f
arfanxn <arfan2173@gmail.com>
2024-11-09 13:30:16 +0700

    add comment

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from flask import Flask
 import singletons
 import config
+import db
 from routes import (web_bp, api_bp)
 
 app = Flask(__name__, instance_relative_config=True)
 
 config.inject_into_app(app)
 singletons.load_jaga_aksara_model()
+
+# * database is configured in the ./models/__init__.py
 
 app.register_blueprint(web_bp)
 app.register_blueprint(api_bp)
