dd2f20247452c2ff7d1ef588df506239b8c2f634
arfanxn <arfan2173@gmail.com>
2024-11-09 13:29:36 +0700

    init db

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,12 @@
+from db import get_db
+import config
+
+db = get_db()
+
 from .User import User
+from .Otp import Otp
 from .PersonalAccessToken import PersonalAccessToken
+
+config.inject_into_db(db)
+
+db.generate_mapping(create_tables=False)
