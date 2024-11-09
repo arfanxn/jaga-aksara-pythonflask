6441ebd5b9e6c3cadb9f443b7191a60634005ec4
arfanxn <arfan2173@gmail.com>
2024-11-10 02:38:38 +0700

    rmoved unused imports

--- routes/api.py
+++ routes/api.py
@@ -1,25 +1,16 @@
-from flask import (Blueprint, g)
-from controllers import (PegonScriptController)
+from flask import (Blueprint)
 from routes.apis.user_routes import user_bp
 from routes.apis.otp_routes import otp_bp
 from routes.apis.article_routes import article_bp
 from routes.apis.transliteration_routes import transliteration_bp
 from routes.apis.chat_routes import chat_bp
 
-
 # Main blueprint
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 # Additional blueprint(s)
-pegon_script_bp = Blueprint('pegon_script', __name__, url_prefix='/pegon-scripts')
 api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(otp_bp)
 api_bp.register_blueprint(article_bp)
 api_bp.register_blueprint(transliteration_bp)
 api_bp.register_blueprint(chat_bp)
-api_bp.register_blueprint(pegon_script_bp)
-
-
-@pegon_script_bp.route('/translate', methods=(['POST']))
-def pegon_script_translate():
-    return PegonScriptController.translate()
