bba5a2313fc4157bb13245c122579da3fefb7fef
arfanxn <arfan2173@gmail.com>
2024-12-01 03:30:05 +0700

    removed authentication on some routes and added authorization on store route

--- routes/apis/article_routes.py
+++ routes/apis/article_routes.py
@@ -1,21 +1,23 @@
 from flask import (Blueprint, g, request)
 from controllers import (ArticleController)
-from middlewares.authentication_middleware import authenticate
+from enums import UserLevelEnum
 from middlewares.app_version_middleware import check_app_version
+from middlewares.authentication_middleware import authenticate
+from middlewares.authorization_middleware import authorize
+
 
 
 article_bp = Blueprint('article', __name__, url_prefix='/articles')
 
-# TODO: only admin can access this route
 @article_bp.route('', methods=(['POST']))
 @check_app_version
 @authenticate
+@authorize(required_user_level=UserLevelEnum.ADMIN.value)
 def store():
     return ArticleController.store()
 
 @article_bp.route('', methods=(['GET']))
 @check_app_version
-@authenticate
 def get():
     if request.args.get('id') is not None: 
         return ArticleController.get()
@@ -24,12 +26,10 @@ def get():
 
 @article_bp.route('/thumbnail', methods=(['GET'])) # '/thumbnail?file=md5_hash'
 @check_app_version
-@authenticate
 def download_thumbnail():
     return ArticleController.download_thumbnail()
 
 @article_bp.route('/content', methods=(['GET'])) # '/content?file=md5_hash'
 @check_app_version
-@authenticate
 def download_content():
     return ArticleController.download_content()
